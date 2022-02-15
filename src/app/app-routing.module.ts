11d61e7831070b967d6adda80b9cd27d6c88175c
chadonet <chadonettzt@gmail.com>
2022-02-15 18:16:58 +0100

    ajout des fichiers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { Course1Component } from './course/course1/course1.component';
+import { Course2Component } from './course/course2/course2.component';
+import { Course3Component } from './course/course3/course3.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"home",component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"course1",component:Course1Component},
+  {path:"course2",component:Course2Component},
+  {path:"course3",component:Course3Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeadingComponent } from './heading/heading.component';
+import { Course1Component } from './course/course1/course1.component';
+import { Course2Component } from './course/course2/course2.component';
+import { Course3Component } from './course/course3/course3.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    HeadingComponent,
+    Course1Component,
+    Course2Component,
+    Course3Component
   ],
   imports: [
     BrowserModule,
